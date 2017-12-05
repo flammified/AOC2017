fcb47935bbf2a653ae83570c699d57eded7e419d
Alexander Freeman <alexander.freeman.nl@gmail.com>
2017-12-05 10:11:41 +0100

    Day5 done

---
+++ day5/src/day5/core.clj
@@ -0,0 +1,27 @@
+(ns day5.core
+  (require [clojure.string :as str]))
+
+(def input (slurp "input-day5.txt"))
+(def test-input)
+
+(defn parse-input [text]
+  (vec (map read-string (str/split-lines text))))
+
+(defn out-of-bounds? [stack position]
+  (or (>= position (count stack)) (< position 0)))
+
+(defn new-offset [offset]
+  (if (>= offset 3)
+    (dec offset)
+    (inc offset)))
+
+(defn steps-to-termination [stack position n]
+  (let [done (out-of-bounds? stack position)]
+    (if done
+      n
+      (let [steps (get stack position)]
+        (recur (update-in stack [position] new-offset) (+ position steps) (inc n))))))
+
+
+(defn -main []
+  (println (steps-to-termination (parse-input input) 0 0)))
