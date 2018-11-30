44e39f3390d6c77e06a00d7aa5d72ab534f05b76
Alexander Freeman <alexander.freeman.nl@gmail.com>
2018-12-01 00:02:43 +0100

    Set up for AOC2018

---
+++ aoc2018/src/day01.clj
@@ -0,0 +1,13 @@
+(ns day01
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as spec]
+            [clojure.edn :as edn]))
+
+
+(def input
+  (-> "day1/input.txt" io/resource io/file slurp))
+
+(defn part-1 [])
+
+(defn part-2 [])
