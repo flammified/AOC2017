c2056fbab27a67690b2298cf354831fa1444bd34
Alexander Freeman <alexander.freeman.nl@gmail.com>
2018-12-01 11:18:04 +0100

    Day1 done! #woot

--- aoc2018/src/day01.clj
+++ aoc2018/src/day01.clj
@@ -4,10 +4,23 @@
             [clojure.spec.alpha :as spec]
             [clojure.edn :as edn]))
 
-
 (def input
-  (-> "day1/input.txt" io/resource io/file slurp))
+  (-> "day01/input.txt"
+      io/resource io/file slurp
+      (->>
+        (str/split-lines)
+        (map edn/read-string))))
 
-(defn part-1 [])
-
-(defn part-2 [])
+(defn part-1 []
+  (reduce + input))
+(defn part-2 []
+  (reduce
+      (fn [{:keys [last-frequency] :as cur} delta]
+        (let [new-frequency (+ last-frequency delta)]
+          (if (some? (get cur new-frequency))
+            (reduced new-frequency)
+            (-> cur
+              (assoc :last-frequency new-frequency)
+              (assoc new-frequency :seen)))))
+    {:last-frequency 0 0 :seen}
+    (cycle input)))
