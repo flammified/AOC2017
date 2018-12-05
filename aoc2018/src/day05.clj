827ecb1e14dfaa7549322845ce486d4ced09b5ec
Alexander Freeman <alexander.freeman.nl@gmail.com>
2018-12-05 17:30:46 +0100

    Day 5 part 2 done

--- aoc2018/src/day05.clj
+++ aoc2018/src/day05.clj
@@ -11,37 +11,45 @@
 
 (def input
   (-> "day05/input.txt"
-      io/resource io/file slurp str/trim))
+      io/resource io/file slurp str/trim seq))
 
 (def lowercase (map char (range 97 123)))
 (def uppercase (map char (range 65 91)))
 
 (def lettermap (merge (zipmap uppercase lowercase) (zipmap lowercase uppercase)))
-(def elements (distinct (map str/lower-case input)))
+(def elements (distinct (flatten (map seq (map str/lower-case input)))))
 
 (defn reacts? [a b]
   (= (lettermap a) b))
 
 (defn react [letters]
-  (loop [tail letters
-         polymer []]
-    (let [[current next-element] (take 2 tail)]
-      (if (p :empty (empty? tail))
-        polymer
-        (if (p :reacts (reacts? current next-element))
-            (recur (p :drop (drop 2 tail)) polymer)
-            (recur (p :drop2 (drop 1 tail)) (conj polymer current)))))))
 
-(defn react-until-end [string]
-  (loop [polymer (vec (seq string))]
-    (let [new-polymer (react polymer)]
-      (if (= new-polymer polymer)
-        (str/join "" polymer)
-        (recur new-polymer)))))
+  (reduce
+    (fn [stack letter]
+      (if (reacts? letter (peek stack))
+        (pop stack)
+        (conj stack letter)))
+    []
+    letters))
+
+  ; Apparently this is slow af?
+  ; (loop [tail letters
+  ;        polymer []]
+  ;   (let [[current next-element] (take 2 tail)]
+  ;     (if (p :empty (empty? tail))
+  ;       polymer
+  ;       (if (p :reacts (reacts? current next-element))
+  ;           (recur (p :drop (drop 2 tail)) polymer)
+  ;           (recur (p :drop2 (drop 1 tail)) (conj polymer current)))))))
 
 (defn part-1 []
   (profile {} (-> input
-                  (react-until-end)
+                  react
                   count)))
 
-(defn part-2 [])
+(defn part-2 []
+  (reduce
+    (fn [best element]
+      (min (->> input (filter #(nil? (#{element (lettermap element)} %))) react count) best))
+    10000
+    elements))
