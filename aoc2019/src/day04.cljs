08706b8b32931b60c1d6caaa07732c2f77cc8c09
Alexander Freeman <afreeman@xebia.com>
2019-12-04 09:55:36 +0100

    Add day4

---
+++ aoc2019/src/day04.cljs
@@ -0,0 +1,41 @@
+(ns day03
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as spec]
+            [clojure.edn :as edn]
+            [clojure.math.numeric-tower :as math]))
+
+(def start 123257)
+(def end 647015)
+
+(defn all-ascending? [s]
+  (->> s
+      (str)
+      (vec)
+      (map str)
+      (map #(Integer/parseInt %))
+      (apply <=)))
+
+
+(defn contains-groups-of-two? [n]
+  (<= 1 (count (filter #(= 2 %) (vals (frequencies (str n)))))))
+
+(defn contains-groups-of-at-least-two? [n]
+  (<= 1 (count (filter #(<= 2 %) (vals (frequencies (str n)))))))
+
+
+(defn correct-1? [s]
+  (and (all-ascending? s)
+       (contains-groups-of-at-least-two? s)))
+
+(defn correct-2? [s]
+  (and (all-ascending? s)
+       (contains-groups-of-two? s)))
+
+(defn part-1 []
+  (println (count (filter correct-1? (range start end)))))
+(defn part-2 []
+  (println (count (filter correct-2? (range start end)))))
+
+(part-1)
+(part-2)
