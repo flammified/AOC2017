f0bc131479b5a4efb69940740443d60a3ff53774
Alexander Freeman <afreeman@xebia.com>
2019-12-08 11:40:40 +0100

    Clean day 8

--- aoc2019/src/day08.clj
+++ aoc2019/src/day08.clj
@@ -9,51 +9,22 @@
 (def input
   (-> "day08/input.txt"
       io/resource io/file slurp str/trim (str/split #"")
-      (->> (mapv edn/read-string) (partition 25) (partition 6))))
-
-
-
-(defn get-xyz [input x y z]
-  (-> input (nth z) (nth x) (nth y)))
-
-(defn set-in-stack [input d w l]
-  (reduce
-    #(assoc %1 %2 (apply (partial get-xyz input) %2))
-    {}
-    (for [z (range d)
-          x (range w)
-          y (range l)]
-      [x y z])))
-
-(defn render-pixel [stack x y d]
-  (first (drop-while
-           #(= 2 %)
-           (map #(get stack %)
-             (for [z (range d)]
-              [x y z])))))
-
-
-(defn render [input]
-  (let [depth (count input)
-        width (count (first input))
-        length (count (first (first input)))]
-
-    (let [stack (set-in-stack input depth width length)]
-      (str/join
-        "\n"
-        (map
-          (fn [x]
-            (apply str
-              (map
-                (fn [y]
-                  (render-pixel stack x y depth))
-                (range length))))
-          (range width))))))
+      (->> (map edn/read-string) (partition (* 25 6)))))
 
 (defn part-1 []
-  (let [input (map #(apply concat %) input)
-        layer (first (sort-by #(count (filter #{0} %)) input))]
+  (let [layer (apply min-key #(count (filter #{0} %)) input)]
     (* (count (filter #{1} layer)) (count (filter #{2} layer)))))
 
 (defn part-2 []
-  (-> (render input) (str/replace  #"0" " ") (str/replace #"1" "█")))
+  (->> input
+      (apply map vector)
+      (map (fn [line] (drop-while #(= 2 %) line)))
+      (map first)
+      (partition 25)
+      (map #(apply str %))
+      (map #(str/replace % #"0" " "))
+      (map #(str/replace % #"1" "█"))
+      (str/join "\n")))
+
+
+(println (part-1))
