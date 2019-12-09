fd1d4d1353c280dadbd11a066a2e5ac07cc07990
Alexander Freeman <afreeman@xebia.com>
2019-12-09 10:53:54 +0100

    Remove amplifiers from day 9

--- aoc2019/src/day09.clj
+++ aoc2019/src/day09.clj
@@ -11,32 +11,6 @@
       (str/split #",")
       (->> (map edn/read-string) (into []))))
 
-
-(defn test-amplifier-setup [program a b c d e]
-  (let [amplifiers {:a (run-async :a program)
-                    :b (run-async :b program)
-                    :c (run-async :c program)
-                    :d (run-async :d program)
-                    :e (run-async :e program)}]
-
-    (>!! (-> amplifiers :a first) a)
-    (>!! (-> amplifiers :b first) b)
-    (>!! (-> amplifiers :c first) c)
-    (>!! (-> amplifiers :d first) d)
-    (>!! (-> amplifiers :e first) e)
-
-    (try
-      (loop [previous 0
-             next (cycle [:a :b :c :d :e])]
-
-        (let [[in out] (get amplifiers (first next))]
-          (do
-            (>!! in previous)
-            (let [output (<!! out)]
-              (if (nil? output)
-                previous
-                (recur output (drop 1 next))))))))))
-
 (defn part-1 []
   (let [inp (chan 1)
         outp (chan 1)]
